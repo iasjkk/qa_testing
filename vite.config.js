289a6bb6cde94372d1024f826eb4b3f0c9ced9b0
iasjkk <iasjkk@gmai.com>
2026-07-25 02:55:54 +0530

    fixed path for github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/qa-testing/',   // replace with your exact GitHub repo name
+  base: '/qa_testing/',   // replace with your exact GitHub repo name
 })
